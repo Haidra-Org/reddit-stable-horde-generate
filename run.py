a13aa5b9f4d620f2ed31c5162cb90d068671f7cb
Divided by Zer0 <mail@dbzer0.com>
2022-12-13 10:35:26 +0100

    test

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ from bot import reddit
 
 @logger.catch(reraise=True)
 def check_for_requests():
-    for comment in reddit.inbox.mentions():
+    for comment in reddit.inbox.all():
         logger.debug([comment.author,comment.body,comment.subreddit,])
 
 
